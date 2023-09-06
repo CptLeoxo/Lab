7d5fdd3d252c9ae85250103bcb2435b39052febc
CptLeoxo <leonova.k@seznam.cz>
2023-09-06 15:37:26 +0200

    Changed python file

---
+++ dice.py
@@ -0,0 +1,16 @@
+import random
+
+class Dice:
+    def dice_roll(self):
+        rolls = []
+        rolls.append(random.randrange(1, 7)) # Add first dice roll
+        rolls.append(random.randrange(1, 7)) # Add seconf dice roll
+        if rolls[0] == rolls[1]: # If index 0 == index 1, add same number to list
+            rolls.append(rolls[0])
+            rolls.append(rolls[0])
+        print(rolls)
+        return rolls
+
+
+kostka = Dice()
+kostka.dice_roll()
