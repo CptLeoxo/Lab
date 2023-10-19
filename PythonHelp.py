8cb8d3b21d0fdefdcf9837a6bac436cbdc2a0c50
CptLeoxo <leonova.k@seznam.cz>
2023-10-19 16:53:59 +0200

    Help

--- PythonHelp.py
+++ PythonHelp.py
@@ -535,5 +535,477 @@ except OSError:
 else:
     print(f"{path} was deleted!")
     
+--------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# Module = a file containing python code. May contain funcs, classess, etc. 
+
+import hello
+hello.hello()
+hello.bye()
+
+import hello as h
+h.hello()
+h.bye()
+
+from hello import *
+hello()
+bye()
+
+help("modules")
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# Rock, Paper, Scissors game
+
+choices = ["rock", "paper", "scissors"]
+computer = random.choice(choices)
+player = None
+
+while player not in choices:
+    player = input("rock/paper/scissors?: ").lower()
+
+if player == computer:
+    print(f"Computer - {computer}")
+    print(f"Player - {player}")
+    print("Tie!")
+elif player == "rock":
+    if computer == "paper":
+        print(f"Computer - {computer}")
+        print(f"Player - {player}")
+        print("Computer wins!")
+    else:
+        print(f"Computer - {computer}")
+        print(f"Player - {player}")
+        print("Player wins!")
+elif player == "scissors":
+    if computer == "rock":
+        print(f"Computer - {computer}")
+        print(f"Player - {player}")
+        print("Computer wins!")
+    else:
+        print(f"Computer - {computer}")
+        print(f"Player - {player}")
+        print("Player wins!")
+elif player == "paper":
+    if computer == "scissors":
+        print(f"Computer - {computer}")
+        print(f"Player - {player}")
+        print("Computer wins!")
+    else:
+        print(f"Computer - {computer}")
+        print(f"Player - {player}")
+        print("Player wins!")
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# Better version
+        
+symbols = ["R", "P", "S"]
+player = input(f"Choose one of the symbols - {symbols}: ").upper()
+ai = random.choice(symbols)
+
+if player == ai:
+    print(f"Plyer: {player} / AI: {ai}")
+    print("Tie!")
+elif (player, ai) in [("R", "S"), ("S", "P"), ("P", "R")]:
+    print(f"Plyer: {player} / AI: {ai}")
+    print("Player won!")
+else:
+    print(f"Plyer: {player} / AI: {ai}")
+    print("AI won!")
+    
+# For a infinity tries of game - while loop 
+# -> while True:
+#  play_again = input("Do you want to play again? yes/no: ").lower()
+#  if play_again != "yes"
+#    break
+#  print("")
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# Basic Quizz Game
+
+def new_game():
+    guesses = []
+    correct_guesses = 0
+    question_num = 1
+    
+    for key in questions:       # The key part is important!
+        print("--------------------------------")
+        print(key)
+        for i in options[question_num-1]:
+            print(i)
+        guess = input(f"Enter A/B/C: ").upper()
+        guesses.append(guess)
+        correct_guesses += check_answer(questions.get(key), guess)
+        question_num += 1
+        
+    display_score(correct_guesses, guess)
+        
+def check_answer(answer, guess):
+    if answer == guess:
+        print("CORRECT")
+        return 1
+    else:
+        print("WRONG")
+        return 0
+
+def display_score(correct_guesses, guesses):
+    print("--------------------------------")
+    print("Results")
+    print("--------------------------------")
+    
+    print("Answers: ", end="")
+    for i in questions:
+        print(questions.get(i), end=" ")
+    print()
+    
+    print("Guesses: ", end="")
+    for i in guesses:
+        print(i, end=" ")
+    print()
+    
+    score = int((correct_guesses/len(questions))*100)
+    print(f"Your score is {score}%.")
+    
+def play_again():
+    response = input("Do you want to play again? Yes/No: ").lower()
+    
+    if response == "yes":
+        return True
+    else:
+        return False
+
+questions = {
+    "Who created Python?: " : "A",
+    "What year was Python created?: " : "B",
+    "Python is tributed to which comedy group?: " : "C",
+    "Is the Earth flat?: " : "A"
+}
+
+options = [["A. Guido van Rossum", "B. Elon Musk", "C. Bill Gates"], 
+           ["A. 1989", "B. 1991", "C. 2016"],
+           ["A. Smosh", "B. Lonely Island", "C. Monty Python"],
+           ["A. True", "B. False", "C. Sometimes"]]
+
+new_game()
+
+while play_again():
+    new_game()
+    
+print("Byeeeeee!")
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# OOP Python
+
+from car import Car
+
+car_1 = Car("Chevy", "Corvette", 2021, "blue")
+car_2 = Car("Ford", "Mustang", 2022, "red")
+
+print(car_1.make, car_1.model, car_1.year, car_1.color)
+print(car_2.make, car_2.model, car_2.year, car_2.color)
+
+car_1.drive()
+car_1.stop()
+car_2.drive()
+car_2.stop()
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# Class variables
+
+from car import Car
+
+car_1 = Car("Chevy", "Corvette", 2021, "blue")
+car_2 = Car("Ford", "Mustang", 2022, "red")
+
+car_1.wheels = 2
+
+print(car_1.make, car_1.color, car_1.wheels)
+print(car_2.make, car_2.color, car_2.wheels)
+print(Car.wheels)
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+class Animal:               # Parent class
+    
+    alive = True
+    
+    def eat(self):
+        print("This animal is eating")
+        
+    def sleep(self):
+        print("This animal is sleeping")
+        
+class Rabbit(Animal):       # Child class from Animal class
+    def run(self):
+        print("This rabbit is running")
+
+class Fish(Animal):
+    def swim(self):
+        print("This fish is swimming")
+
+class Hawk(Animal):
+    def fly(self):
+        print("This hawk is flying")
+
+rabbit = Rabbit()
+fish = Fish()
+hawk = Hawk()
+
+print(rabbit.alive)
+hawk.sleep()
+fish.eat()
+hawk.fly()
+fish.swim()
+rabbit.run()
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# Multi-level inheritance when a derived (child) class inherits another derived (child) class
+
+class Organism:
+    
+    alive = True
+    
+class Animal(Organism):
+    
+    def eat(self):
+        print("This animal is eating")
+        
+class Dog(Animal):
+    
+    def bark(self):
+        print("This dog is barking")
+        
+dog = Dog()
+print(dog.alive)
+dog.eat()
+dog.bark()
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# Multiple inheritance = when a child class is derived from more than one parent class
+
+class Prey:
+    
+    def flee(self):
+        print("This animal flees")
+        
+class Predator:
+    
+    def hunt(self):
+        print("This animal is hunting")
+        
+class Rabbit(Prey):
+    pass
+
+class Hawk(Predator):
+    pass
+
+class Fish(Prey, Predator):
+    pass
+
+rabbit = Rabbit()
+hawk = Hawk()
+fish = Fish()
+
+rabbit.flee()
+hawk.hunt()
+fish.flee()
+fish.hunt()
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# Method overwriting
+
+class Animal:
+    
+    def eat(self):
+        print("This animal is eating")
+        
+class Rabbit(Animal):
+    
+    def eat(self):
+        print("This rabbit is eating a carrot")
+
+rabbit = Rabbit()
+rabbit.eat()
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# Method chaining = calling multiple methods sequentially. Each call performs an action on the same object and returns self
+
+class Car:
+    
+    def turn_on(self):
+        print("You start the engine")
+        return self   
+    
+    def drive(self):
+        print("You drive the car")
+        return self
+    
+    def brake(self):
+        print("You step on the brakes")
+        return self
+    
+    def turn_off(self):
+        print("You turn off the engine")
+        return self
+    
+car = Car()
+car.turn_on()
+car.drive()
+
+car.turn_on().drive()           # Need to return self in funcs
+
+car.turn_on()\
+    .drive()\
+        .brake()\
+            .turn_off()
+            
+--------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# super() = Function used to give access to the methods of a parent class. Returns a temporary object a parent class when used.
+
+class Rectangle:
+    
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+        
+class Square(Rectangle):
+    
+    def __init__(self, length, width):
+        super().__init__(length, width)
+        
+    def area(self):
+        return self.length*self.width
+    
+class Cube(Rectangle):
+    
+    def __init__(self, length, width, height):
+        super().__init__(length, width)
+        self.height = height
+        
+    def volume(self):
+        return self.length*self.width*self.height
+        
+square = Square(3, 3)
+cube = Cube(3, 3, 3)
+print(square.area())
+print(cube.volume())
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# Abstract classes = prevents a user from creating an object of that class + compels a user to override abstract methods in a child class
+# abstract class = a class which contains one or more abstract methods
+# abstract method = a method that has a declaration but does not have an implementation
+
+from abc import ABC, abstractmethod
+
+class Vehicle(ABC):
+    
+    @abstractmethod
+    def go(self):
+        pass
+    
+class Car(Vehicle):
+    
+    def go(self):
+        print("You drive the car")
+        
+class Motorcycle(Vehicle):
+    
+    def go(self):
+        print("You ride the motorcycle")
+        
+vehicle = Vehicle()
+car = Car()
+motorcycle = Motorcycle()
+
+vehicle.go()
+car.go()
+motorcycle.go()
+
+
+
+
 """
+
+class Pes:
+
+    krmivo = ['granule', 'maso', 'gauc']            #verejny datovy clen tridy
+
+    def __init__(self, jmeno, majitele, zvuk_stekani):
+        self._jmeno = jmeno                         #chraneny datovy clen instance
+        self.majitele = majitele                    #verejny datovy clen instance
+        self.__zvuk_stekani = zvuk_stekani          #privatni datovy clen instance
+        self._vek = 0
+        self._prikaz = None
+
+    def stekej(self):                               #veřejná metoda instance
+        return self.__zvuk_stekani                    
+
+    def __zmen_zvuk(self, novy_zvuk):               #privatní metoda instance
+        self.__zvuk_stekani = novy_zvuk               
+
+    #jmeno bude read-write - jmeno muzeme menit a pes nam ho obcas i prozradi nebo stekne :)
+    @property                                       #definice verejne vlastnosti (accessor)
+    def jmeno(self):                                
+        return self._jmeno if random.random() > 0.5 else "Haf???"
+
+    @jmeno.setter                                   #definice verejne vlastnosti (mutator)
+    def jmeno(self, value):
+        if value != "Jonatan":                      #pes se vsak nechce jmenovat Jonatan :))
+            self._jmeno = value
+
+    #vek bude read-only - vek nemuzeme jako verejnost nastavit, pes musi starnout
+    @property
+    def vek(self):
+        self._vek += 1
+        if self._vek >= 5:
+            self.__zmen_zvuk("GRAAAAWWWWW HAAAAAF VRRRRR")
+        return self._vek
+
+    #prikaz bude write-only (pes nam nemuze rict, co jsme mu prikazali)
+    @property
+    def prikaz(self):
+        raise AttributeError('unreadable attribute')
+
+    @prikaz.setter
+    def prikaz(self, value):
+        self._prikaz = value
+
+    @classmethod                            
+    def pridej_krmivo(cls, krmivo):                 #verejna metoda tridy
+        cls.krmivo.append(krmivo)           
+
+    @staticmethod
+    def jak_dela_pes():                             #verejna staticka metoda
+        return "haf haf"                    
+
+azor = Pes(jmeno="Azor", majitele=None, zvuk_stekani="haf haf mnau?")
+print(azor.stekej())
+
+print(azor.__zmen_zvuk("haf"))  #volani privatni metody instance
+
+azor.jmeno = "Jonatan"  #nastaveni nove hodnoty mutatorem
+print(azor.jmeno)       #volani hodnoty accessorem
+azor.jmeno = "Rex"
+print(azor.jmeno)
+
+print(azor._vek)                #volani chranene promenne instance (bohuzel Python umozni)
+
+azor.vek = 20                   #nastaveni read-only vlastnosti
+
+print(azor.vek)                 #volani read-only vlastnosti, ktera internet nastavuje privatni promennou
+print(azor.stekej())            #volani verejne metody instance
+
+azor.prikaz = "k noze"          #nastaveni write-only vlastnosti
+
+print(azor.prikaz)              #cteni write-only vlastnosti
 
